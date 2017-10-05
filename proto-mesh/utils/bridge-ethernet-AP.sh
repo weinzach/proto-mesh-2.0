a0a78e22c3a5619cfe733b05d6e47ecbd4c7e23c
zdog1230 <zdog1230@yahoo.com>
2017-10-05 00:10:54 +0000

    Remove batman related utils

--- proto-mesh/utils/bridge-ethernet-AP.sh
+++ proto-mesh/utils/bridge-ethernet-AP.sh
@@ -7,7 +7,7 @@
 
 sudo ip link add name mesh-bridge type bridge
 sudo ip link set dev eth0 master mesh-bridge
-sudo ip link set dev bat0 master mesh-bridge
+sudo ip link set dev br0 master mesh-bridge
 sudo ip link set up dev eth0
-sudo ip link set up dev bat0
+sudo ip link set up dev br0
 sudo ip link set up dev mesh-bridge
--- proto-mesh/utils/countNodes.sh
+++
@@ -1,1 +0,0 @@
-sudo batctl o -H | wc -l | awk '{print expr $1 + 1}'
